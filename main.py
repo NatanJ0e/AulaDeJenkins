c11982ac7cf180a24f4029ba6c78ed95cc749062
NatanJ0e <natanael_ferreira2@hotmail.com>
2021-06-15 19:05:07 -0300

    gitignore file

---
+++ main.py
@@ -0,0 +1,13 @@
+import selenium
+import selenium.webdriver.chrome
+import unittest
+
+valorEntrada = 200
+valorSaida = 5000
+salario = 5000
+
+if salario == valorSaida:
+    print('certo')
+else:
+    print('errado')
+
